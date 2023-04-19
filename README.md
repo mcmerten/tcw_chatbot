ea8a18ecaaa4e04a83c845de3bcfee1120d8b6d6
Merten Bülter <merten.buelter@tum.de>
2023-04-19 17:43:53 +0200

    refactored scraper

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
----
-title: Tcw Chatbot
-emoji: 😻
-colorFrom: green
-colorTo: pink
-sdk: gradio
-sdk_version: 3.27.0
-app_file: app.py
-pinned: false
----
-
-Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
-# tcw_chatbot
+# TCW Chatbot V1 
--- chatbot.py
+++ chatbot.py
@@ -9,17 +9,19 @@ from langchain.prompts import PromptTemplate#
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQAWithSourcesChain, RetrievalQA, ConversationalRetrievalChain
 from langchain import OpenAI
+from langchain.memory import ConversationBufferMemory
+
 import gradio as gr
 
 load_dotenv()
 os.environ['OPENAI_API_KEY'] = os.environ.get('OPENAI_API_TOKEN')
 
 embeddings = OpenAIEmbeddings()
-db = Chroma(persist_directory="db", embedding_function=embeddings)
+db = Chroma(persist_directory="chroma_db", embedding_function=embeddings, collection_name="tcw_chroma_collection")
 
-prompt_template = """Use the following pieces of context to answer the question at the end.
+# Tune prompt to give it more the sense of a chatbot 
+prompt_template = """You are a chatbot used on a corporate website. Be helpful. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
-In case the question has multiple answers, formulate the answer in bullet points. 
 
 {context}
 
@@ -30,22 +32,26 @@ PROMPT = PromptTemplate(
 ) 
 
 chain_type_kwargs = {"prompt": PROMPT}
+
 #qa = RetrievalQA.from_chain_type(llm=OpenAI(),
 #                                 chain_type="stuff",
 #                                 retriever=db.as_retriever(),
 #                                 chain_type_kwargs=chain_type_kwargs, 
 #                                 return_source_documents=True)
 
-qa = ConversationalRetrievalChain.from_llm(llm = OpenAI(temperature=0),
-                                                 retriever = db.as_retriever(),
-                                                 qa_prompt=PROMPT,
-                                                 chain_type="stuff", 
-                                                 return_source_documents = True)
 chat_history = []
+#memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-########
+qa = ConversationalRetrievalChain.from_llm(llm = OpenAI(temperature=0),
+                                           retriever = db.as_retriever(),
+                                           qa_prompt=PROMPT,
+                                           chain_type="stuff", 
+                                           return_source_documents = False, 
+                                           #memory = memory, 
+                                           verbose = True)
 
 def qa_query(query, chat_history):
+    chat_history = []
     result = qa({"question": query, "chat_history" : chat_history})
     return result
 
@@ -61,9 +67,8 @@ with gr.Blocks() as tcw_bot:
         query = history[-1][0]
         response = qa_query(query, chat_history)
         print(response)
-        chat_history.append((query, response["answer"]))
+        #chat_history.append((query, response["answer"]))
         response_result = response["answer"]
-        #metadata = response["source_documents"]
         history[-1][1] = response_result
         return history
 
